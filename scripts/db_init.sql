1a4fba75f6e04c7a73a803d940666cb04a63f1ad
Oleksandr Shcherbyna <1928072+alscherbina@users.noreply.github.com>
2019-10-27 19:25:24 +0200

    Comments cleanup

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -34,34 +34,3 @@ CREATE TABLE booking (
 CREATE INDEX booking_offer_id ON booking ( offer_id );
 CREATE INDEX booking_start_date ON booking ( start_date );
 CREATE INDEX booking_end_date ON booking ( end_date );
-/*
-CREATE TABLE task (
-  id SERIAL PRIMARY KEY,
-  type VARCHAR(24) NOT NULL,
-  url VARCHAR(512) NOT NULL UNIQUE,
-  schedule VARCHAR(32) NOT NULL,
-  title VARCHAR(256),
-  active BOOLEAN NOT NULL DEFAULT TRUE,
-  creation_date TIMESTAMPTZ DEFAULT NOW(TIMESTAMPTZ DEFAULT NOW());
-
-CREATE TABLE journal (
-    task_id INTEGER NOT NULL,
-    execution_date TIMESTAMPTZ DEFAULT NOW(),
-    result VARCHAR(256),
-    CONSTRAINT unique_journal_record UNIQUE(task_id, execution_date),
-    CONSTRAINT fk_journal_task FOREIGN KEY (task_id) REFERENCES task(id) ON DELETE CASCADE
-  )
-  INSERT INTO
-    public.task (
-      type,
-      url,
-      schedule,
-      title
-    )
-  VALUES(
-      'rozetka',
-      'https://hard.rozetka.com.ua/dell_210_anvr/p41964776/',
-      '0 0/5 * 1/1 * ? *',
-      'Монитор 34" Dell P3418HW '
-    );
-*/
